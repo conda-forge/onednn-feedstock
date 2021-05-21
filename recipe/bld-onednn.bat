229ead602094fbdd094dacd70dbd4c70b943b391
Safonov, Igor <igor.safonov@intel.com>
2021-05-20 19:23:40 -0700

    windows: add omp

--- recipe/bld-onednn.bat
+++ recipe/bld-onednn.bat
@@ -2,11 +2,16 @@
 
 md "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
-set TBBROOT=%PREFIX%
 set CMAKE_PREFIX_PATH=%PREFIX%
+if [%dnnl_cpu_runtime%]==[tbb] (
+    set TBBROOT=%PREFIX%
+    set DNNL_CPU_RUNTIME=TBB
+    )
+if [%dnnl_cpu_runtime%]==[omp] set DNNL_CPU_RUNTIME=OMP
+
 cmake -GNinja %CMAKE_ARGS% ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-  -DDNNL_CPU_RUNTIME=TBB ^
+  -DDNNL_CPU_RUNTIME=%DNNL_CPU_RUNTIME% ^
   -DDNNL_GPU_RUNTIME=NONE ^
   ..
 if errorlevel 1 exit 1
